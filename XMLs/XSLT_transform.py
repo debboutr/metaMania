361be0c92130cebfa8cc8edf6f4363e2f802f684
rickD <debboutr@gmail.com>
2017-01-06 10:38:55 -0800

    initial commit

---
+++ XMLs/XSLT_transform.py
@@ -0,0 +1,13 @@
+##Make sure that the workspace has all of the xmls you want to transform as well as the FGDC Plus.xsl file in that location
+import os, sys, arcpy
+from arcpy import env
+
+work_dir = "L:/Priv/CORFiles/Geospatial_Library/Data/Project/StreamCat/FTP_Staging/StreamCat/Documentation/Metadata/XMLs"
+env.workspace = work_dir
+html_dir = "L:/Priv/CORFiles/Geospatial_Library/Data/Project/StreamCat/FTP_Staging/StreamCat/Documentation/Metadata"
+xslt = "L:/Priv/CORFiles/Geospatial_Library/Data/Project/SSWR1.1B/Metadata/FGDC Plus.xsl"
+for xml in arcpy.ListFiles("*.xml"):
+    html = html_dir + '/' + xml.split(".")[0] + '.html'    
+    if not os.path.exists(html):
+        print html
+        arcpy.XSLTransform_conversion(xml, xslt, html)
---
+++ XMLs/work/Bold_Glossary_README.txt
@@ -0,0 +1,3 @@
+After creating the html using the xsl transform, open it in a text editor and (ctrl+F)ind 'Glossary of Terms:' in the Abstract.  Just copy and paste the line below over the existing text to make bold and save.
+
+</pre></div><div class="md"><b>Glossary of Terms: </b><pre id="fixvalue">
